aee56ffbe9f48e6fc77adb612c39ef9a00ccfd27
tatsui <2360691+tatsu-i@users.noreply.github.com>
2019-05-18 12:29:31 +0000

    update plaso

--- plaso/entrypoint.sh
+++ plaso/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-log2timeline.py output.plaso /data/*
+log2timeline.py --partitions all --vss_stores all --workers ${WORKER_NUM:-2} /data/output.plaso /data/*.E01
 # HELP list timezone
 # psort.py -z list
-psort.py -o json_line -z Japan -a -w /data/output.json output.plaso
+psort.py -o json_line -z Japan -a -w /data/output.json /data/output.plaso
